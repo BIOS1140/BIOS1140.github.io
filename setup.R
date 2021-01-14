0a5a733374f2233a8f1925b4b46324c6a6d479c4
evengar <even.garvang@gmail.com>
2021-01-14 17:01:03 +0100

    moved css and setup chunk to their own files

---
+++ setup.R
@@ -0,0 +1,8 @@
+knitr::opts_chunk$set(echo = TRUE, 
+                      fig.width = 5, 
+                      fig.asp = 3/4, 
+                      comment = "#>",
+                      warning = FALSE,
+                      message = FALSE,
+                      collapse = TRUE)
+library(tidyverse)
